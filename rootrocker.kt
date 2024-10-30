6b6be16b95c44746bcdd185dff12edf2754fb2b1
sojoyork <aimaankhankvsworkmail@gmail.com>
2024-10-30 00:45:10 -0700

    Update rootrocker.kt

--- rootrocker.kt
+++ rootrocker.kt
@@ -1,7 +1,6 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-// Function to run ADB commands
 fun runCommand(command: String): String {
     val processBuilder = ProcessBuilder(command.split(" "))
     val process = processBuilder.start()
@@ -17,60 +16,25 @@ fun runCommand(command: String): String {
     return output.toString()
 }
 
-// RootRockerDestroyer: Soft factory reset feature
-fun rootRockerDestroyer() {
-    println("Would you like to perform a soft factory reset? (y/n): ")
-    val userInput = readLine()!!
-
-    if (userInput.lowercase() == "y") {
-        println("Starting soft factory reset...")
-        val resetOutput = runCommand("adb shell am broadcast -a android.intent.action.MASTER_CLEAR")
-        println("Soft factory reset completed: \n$resetOutput")
-    } else {
-        println("Soft factory reset canceled.")
-    }
-}
-
 fun main() {
     println("###################################################")
-    println("### Welcome to RootRocker! 🎸                  ###")
-    println("### Let's start rocking!                       ###")
+    println("### Welcome to RootRocker 🎸!                   ###")
+    println("### Let's start rooting!                        ###")
     println("###################################################")
 
-    // Simulated device detection
+    // Check connected devices
     val deviceList = runCommand("adb devices")
     println("Connected Devices: \n$deviceList")
 
     if (deviceList.contains("device")) {
-        // Device detected, fetch device info, and root
-        fetchDeviceInfo()
+        println("Device detected. Proceeding with rooting...")
 
-        // Engage RootRocker ROCKSTAR defense
-        if (!rootRockerRockstar()) {
-            println("Rooting process terminated due to security concerns.")
-            return
-        }
+        // Example: Reboot the device to bootloader (to flash custom recovery)
+        val rebootOutput = runCommand("adb reboot bootloader")
+        println("Rebooting to bootloader: \n$rebootOutput")
 
-        // Proceed with rooting process
-        println("Proceeding with rooting...")
-
-        val pushSu = runCommand("adb push path_to_su_binary /system/xbin/su")
-        println("Pushing SU binary: \n$pushSu")
-
-        val chmodSu = runCommand("adb shell chmod 0755 /system/xbin/su")
-        println("Setting SU binary permissions: \n$chmodSu")
-
-        // Verify root
-        if (verifyRoot()) {
-            println("Root access confirmed! The device is now rooted.")
-        } else {
-            println("Root access failed. Please try again.")
-        }
-
-        // Offer the option for a soft factory reset
-        rootRockerDestroyer()
-
+        // Here you would add commands to install TWRP, root binaries, etc.
     } else {
-        println("No device connected. Please connect a Samsung device via USB.")
+        println("No device connected. Please connect a device via USB.")
     }
 }
